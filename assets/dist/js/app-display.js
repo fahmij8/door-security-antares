7cc4d3f7c85a7affdbe37fd69e97db0eae7724a5
fahmij8 <fahmijabbar@upi.edu>
2021-08-21 03:56:01 +0700

    Add microcontroller status

--- assets/dist/js/app-display.js
+++ assets/dist/js/app-display.js
@@ -42,20 +42,66 @@ const dashboardCheckDeviceStatus = async () => {
     $(".dash-check").addClass("disabled").html("Please wait ...");
     let lastIP = await getData(2);
     let streamUrl = `http://${lastIP.IP}`;
-    fetch(streamUrl + "/status")
-        .then((res) => res.text())
-        .then((text) => {
-            $(".dash-cam-border").removeClass("border-left-danger").addClass("border-left-success");
-            $(".dash-cam-text").removeClass("text-danger").addClass("text-success");
-            $(".dash-cam-stat").html("Activated");
-        })
-        .catch((error) => {
-            $(".dash-cam-border").removeClass("border-left-success").addClass("border-left-danger");
-            $(".dash-cam-text").removeClass("text-success").addClass("text-danger");
-            $(".dash-cam-stat").html("Deactivated");
-        });
-    $(".dash-lu").html(`Last system up checked : ${new Date().toLocaleString()}`);
-    $(".dash-check").removeClass("disabled").html("Check Status");
+    Promise.all([
+        await fetch(streamUrl + "/status")
+            .then((res) => res.text())
+            .then(() => {
+                $(".dash-cam-border").removeClass("border-left-danger").addClass("border-left-success");
+                $(".dash-cam-text").removeClass("text-danger").addClass("text-success");
+                $(".dash-cam-stat").html("Activated");
+            })
+            .catch((error) => {
+                console.error(error);
+                $(".dash-cam-border").removeClass("border-left-success").addClass("border-left-danger");
+                $(".dash-cam-text").removeClass("text-success").addClass("text-danger");
+                $(".dash-cam-stat").html("Deactivated");
+            }),
+        await getData(4, "unparsed")
+            .then((data) => {
+                let microcontrollerStatus = JSON.parse(JSON.parse(data));
+                let lastUpTime = microcontrollerStatus["m2m:cin"]["lt"];
+                let isUp = false;
+                console.log(lastUpTime);
+                let padZeroFront = (number) => {
+                    if (typeof number === "number") {
+                        number = number.toString();
+                        if (number.length === 1) {
+                            number = "0" + number;
+                        }
+                    }
+                    return number;
+                };
+                if (new Date().getFullYear() === parseInt(lastUpTime.slice(0, 4))) {
+                    if (padZeroFront(new Date().getMonth() + 1) === lastUpTime.slice(4, 6)) {
+                        if (padZeroFront(new Date().getDate()) === lastUpTime.slice(6, 8)) {
+                            if (padZeroFront(new Date().getHours()) === lastUpTime.slice(9, 11)) {
+                                if (new Date().getMinutes() - 20 <= parseInt(lastUpTime.slice(11, 13))) {
+                                    isUp = true;
+                                }
+                            }
+                        }
+                    }
+                }
+                if (isUp) {
+                    $(".dash-esp-border").removeClass("border-left-danger").addClass("border-left-success");
+                    $(".dash-esp-text").removeClass("text-danger").addClass("text-success");
+                    $(".dash-esp-stat").html("Activated");
+                } else {
+                    $(".dash-esp-border").removeClass("border-left-success").addClass("border-left-danger");
+                    $(".dash-esp-text").removeClass("text-success").addClass("text-danger");
+                    $(".dash-esp-stat").html("Deactivated");
+                }
+            })
+            .catch((error) => {
+                console.error(error);
+                $(".dash-esp-border").removeClass("border-left-success").addClass("border-left-danger");
+                $(".dash-esp-text").removeClass("text-success").addClass("text-danger");
+                $(".dash-esp-stat").html("Error!");
+            }),
+    ]).then(() => {
+        $(".dash-lu").html(`Last system up checked : ${new Date().toLocaleString()}`);
+        $(".dash-check").removeClass("disabled").html("Check Status");
+    });
 };
 
 export { sbInit, Toast, dashboardCheckDeviceStatus };
